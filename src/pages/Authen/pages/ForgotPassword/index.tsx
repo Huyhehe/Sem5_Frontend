6e79b4595b7defeadcd44a43e2e4201e72ff4480
Huyhehe <thhyy2002@gmail.com>
2022-11-11 21:52:41 +0700

    chore: change file structure of Authen folder

---
+++ src/pages/Authen/pages/ForgotPassword/index.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react"
+
+interface ForgotPasswordProps {}
+
+const ForgotPassword: FunctionComponent<ForgotPasswordProps> = () => {
+  return <div>ForgotPassword page</div>
+}
+
+export default ForgotPassword
