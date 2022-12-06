f1c23f43ef106ec07305bd98575a3e25986a3696
Huyhehe <thhyy2002@gmail.com>
2022-12-07 00:33:46 +0700

    refactor: change the logic of http requests

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,8 +1,19 @@
-import axiosInstance from "@/config/axiosConfig"
 import axios from "axios"
 
 const API_URL = import.meta.env.VITE_API
+let axiosInstance: any = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    common: {
+      Authorization: `Bearer ${JSON.parse(
+        localStorage.getItem("accessToken") as string
+      )}`,
+    },
+  },
+})
 
+// AUTH
 export const getAllLocationReviews = async () => {
   try {
     const res = await axiosInstance.get(`${API_URL}/reviews`)
@@ -22,6 +33,15 @@ export const getLocationReviewById = async (id: number) => {
 export const signInAPI = async (user: { email: string; password: string }) => {
   try {
     const res = await axios.post(`${API_URL}/auth/login/`, user)
+    axiosInstance = axios.create({
+      baseURL: API_URL,
+      headers: {
+        "Content-Type": "application/json",
+        common: {
+          Authorization: `Bearer ${res.data.access_token}`,
+        },
+      },
+    })
     return res.data
   } catch (error: any) {
     throw new Error(error.response.data.error)
@@ -48,7 +68,7 @@ export const registerAPI = async (user: any) => {
     const res = await axios.post(`${API_URL}/auth/register/`, user)
     return res.data
   } catch (error: any) {
-    throw new Error(error.response.data.error)
+    throw new Error(error.response.data.email[0])
   }
 }
 export const verifyEmailAPI = async (token: string) => {
@@ -56,14 +76,16 @@ export const verifyEmailAPI = async (token: string) => {
     const res = await axios.get(`${API_URL}/auth/email-verify/${token}`)
     return res.data
   } catch (error: any) {
-    throw new Error(error.response.data.error)
+    throw new Error(error.response.data.error || error.message)
   }
 }
 
-// export const verifyAPI = async (code: string) => {
-//   try {
-//     const response =
-//   } catch (error: any) {
-//     throw new Error(error)
-//   }
-// }
+// USER
+export const getUserAPI = async (id: string) => {
+  try {
+    const res = await axiosInstance.get(`/users/get-user/${id}`)
+    return res.data
+  } catch (error: any) {
+    throw new Error(error.response.data.messages[0].message)
+  }
+}
