5eac1b673765301cb2c93adf5aa4c58c7e48d4f3
Huyhehe <thhyy2002@gmail.com>
2022-12-07 00:34:46 +0700

    fix: fix the bug causing null accessToken

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,9 +1,7 @@
 const useUser = () => {
   if (localStorage.getItem("user") !== null) {
-    const { username, accessToken } = JSON.parse(
-      localStorage.getItem("user") as string
-    )
-    return { username, accessToken }
+    const user = JSON.parse(localStorage.getItem("user") as string)
+    return user
   }
   return null
 }
