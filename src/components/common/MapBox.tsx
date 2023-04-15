d898e0a57f9e8a557c904be44353547a8f71984a
Huyhehe <thhyy2002@gmail.com>
2023-04-15 11:17:21 +0700

    feat: [TC-52] fix map can't be moved

--- src/components/common/MapBox.tsx
+++ src/components/common/MapBox.tsx
@@ -1,6 +1,12 @@
 import useCoord from "@/hooks/useCoord"
 import { mapBoxGlAPI } from "@/utils/constant"
-import Map, { FullscreenControl, GeolocateControl, Marker } from "react-map-gl"
+import { useEffect, useState } from "react"
+import Map, {
+  FullscreenControl,
+  GeolocateControl,
+  Marker,
+  ViewState,
+} from "react-map-gl"
 
 interface MapBoxProps {
   address: string
@@ -8,17 +14,41 @@ interface MapBoxProps {
 
 const MapBox = ({ address }: MapBoxProps) => {
   const [latitude, longitude] = useCoord(address)
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude,
+    longitude,
+    zoom: 13,
+  })
+
+  useEffect(() => {
+    setViewport({
+      ...viewport,
+      latitude,
+      longitude,
+    })
+  }, [latitude, longitude])
+
+  const handleOnMove = (viewState: ViewState) => {
+    setViewport((prev) => {
+      return {
+        ...prev,
+        ...viewState,
+      }
+    })
+  }
+
   return (
     <Map
-      initialViewState={{
-        zoom: 13,
-      }}
-      latitude={latitude}
-      longitude={longitude}
+      {...viewport}
       mapStyle="mapbox://styles/mapbox/streets-v12"
       mapboxAccessToken={mapBoxGlAPI}
       scrollZoom
       doubleClickZoom
+      onMove={({ viewState }) => {
+        handleOnMove(viewState)
+      }}
     >
       <Marker longitude={longitude} latitude={latitude} color="red" />
       <GeolocateControl />
